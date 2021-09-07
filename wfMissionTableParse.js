161d05ba4748531aefb51d0feeed2348ab8117b0
sinearc <fossilizedcrinoids@gmail.com>
2021-09-07 16:02:30 -0400

    modification to mission parse that lets it handle the transient reward tables

--- wfMissionTableParse.js
+++ wfMissionTableParse.js
@@ -10,100 +10,106 @@ function cleanNonZeroFalsyProperties(item) {
     return newObj
 }
 
-let missionTable = Array.from(document.getElementById("missionRewards").nextElementSibling.children[0].children)
-console.log(missionTable)
-let missionTableNoFalsyProps = missionTable.map(cleanNonZeroFalsyProperties)
+function MissionTableParse (id) {
+    let missionTable = Array.from(document.getElementById(id).nextElementSibling.children[0].children)
+    console.log(missionTable)
+    let missionTableNoFalsyProps = missionTable.map(cleanNonZeroFalsyProperties)
 
-let missionTablePairedChances = missionTableNoFalsyProps.map(item => {
-    if (item.childElementCount === 2) {
-        return {
-            "drop": item.children[0].innerText,
-            "chance": item.children[1].innerText,
-            "classList": []
-        }
-    } else if (item.childElementCount === 1 && !item.classList.length) {
-        return cleanNonZeroFalsyProperties(item.children[0])
-    } else {
-        return item
-    }
-})
-
-console.log(missionTableNoFalsyProps)
-let individualTablesNumber = missionTableNoFalsyProps.filter((item) => item.classList.length ? true : false)
-let currentArray = missionTablePairedChances
-let individualTableSlices = []
-for (item of individualTablesNumber) {
-    let arr = []
-    for (row of currentArray) {
-        if (row.classList.length) {
-            let lastIndex = currentArray.indexOf(row)
-            individualTableSlices.push(arr)
-            currentArray = currentArray.slice(lastIndex + 1)
-            break
+    let missionTablePairedChances = missionTableNoFalsyProps.map(item => {
+        if (item.childElementCount === 2 && item.children[0].innerText && item.children[1].innerText) {
+            return {
+                "drop": item.children[0].innerText,
+                "chance": item.children[1].innerText,
+                "classList": []
+            }                
+        } else if (!item.classList.length) {
+            return cleanNonZeroFalsyProperties(item.children[0])
         } else {
-            arr.push(row)
+            return item
         }
-    }
-}
-console.log(individualTableSlices)
-
-individualTableSlices.forEach(list => {
-    list.forEach(item => {
-        delete item.classList
     })
-})
 
-let individualTableRotations = individualTableSlices.map(slice => {
-    let possibleRotations = [ "A", "B", "C" ]
-    let rotations = []
-    let returnObj = {
-        "location": slice.shift().innerText,
-        "rotations": null,
-        "rotationList": [],
-        "drops": []
-    }
-    let rotationNumber = slice.filter(item => item.localName === "th" ? true : false)
-    let currentArray = slice.reverse()
-    if (rotationNumber.length) {
-        returnObj.rotations = rotationNumber.length
-        for (each of rotationNumber) {
-            const whichRotation = {
-                "rotation": null,
-                "drops": []
+    console.log(missionTableNoFalsyProps)
+    let individualTablesNumber = missionTableNoFalsyProps.filter((item) => item.classList.length ? true : false)
+    let currentArray = missionTablePairedChances
+    let individualTableSlices = []
+    console.log(individualTablesNumber)
+    console.log(missionTablePairedChances)
+    for (item of individualTablesNumber) {
+        let arr = []
+        for (row of currentArray) {
+            if (row.classList.length) {
+                let lastIndex = currentArray.indexOf(row)
+                individualTableSlices.push(arr)
+                currentArray = currentArray.slice(lastIndex + 1)
+                break
+            } else {
+                arr.push(row)
             }
-            for (row of currentArray) {
-                if (row.localName === 'th') {
-                    whichRotation.rotation = row.innerText
-                    currentArray = currentArray.slice(currentArray.indexOf(row) + 1)
-                    break
-                } else {
-                    whichRotation.drops.push(row)
-                }
-            }
-            rotations.push(whichRotation)
         }
-        returnObj.rotationList = rotations.reverse()
-        return returnObj        
-    } else {
-        returnObj.drops = slice.reverse()
-        return returnObj
     }
-})
+    console.log(individualTableSlices)
 
-individualTableRotations.forEach(each => {
-    if (each.rotations) {
-        each.rotationList.forEach(r => {
-            r.drops.forEach(drop => {
-                drop.rotation = r.rotation
-                each.drops.push(drop)
+    individualTableSlices.forEach(list => {
+        list.forEach(item => {
+            delete item.classList
+        })
+    })
+
+    let individualTableRotations = individualTableSlices.map(slice => {
+        let possibleRotations = [ "A", "B", "C" ]
+        let rotations = []
+        let returnObj = {
+            "location": slice.shift().innerText,
+            "rotations": null,
+            "rotationList": [],
+            "drops": []
+        }
+        let rotationNumber = slice.filter(item => item.localName === "th" ? true : false)
+        let currentArray = slice.reverse()
+        if (rotationNumber.length) {
+            returnObj.rotations = rotationNumber.length
+            for (each of rotationNumber) {
+                const whichRotation = {
+                    "rotation": null,
+                    "drops": []
+                }
+                for (row of currentArray) {
+                    if (row.localName === 'th') {
+                        whichRotation.rotation = row.innerText
+                        currentArray = currentArray.slice(currentArray.indexOf(row) + 1)
+                        break
+                    } else {
+                        whichRotation.drops.push(row)
+                    }
+                }
+                rotations.push(whichRotation)
+            }
+            returnObj.rotationList = rotations.reverse()
+            return returnObj        
+        } else {
+            returnObj.drops = slice.reverse()
+            return returnObj
+        }
+    })
+
+    individualTableRotations.forEach(each => {
+        if (each.rotations) {
+            each.rotationList.forEach(r => {
+                r.drops.forEach(drop => {
+                    drop.rotation = r.rotation
+                    each.drops.push(drop)
+                })
             })
-        })
-    }
-    else {
-        each.drops.forEach(drop => {
-            drop.rotation = null
-        })
-    }
-})
+        }
+        else {
+            each.drops.forEach(drop => {
+                drop.rotation = null
+            })
+        }
+    })
+    return individualTableRotations
+}
 
-console.log(individualTableRotations)
+
+console.log(MissionTableParse('missionRewards'))
